40e5a794d64452702f12805a69e09ba9c25cd989
Hollyys <crossrunway01@gmail.com>
2024-03-14 22:04:16 +0900

    240312

--- python/practice/15.py
+++ python/practice/15.py
@@ -1,1 +1,12 @@
-print("hello")
+from collections import deque
+
+def solution(N, K):
+    q = deque(range(1,N+1))
+    while len(q) > 1:
+        for i in range(K-1):
+            q.append(q.popleft())
+        q.popleft()
+
+    return q.popleft()
+    
+print(solution(5,2))
