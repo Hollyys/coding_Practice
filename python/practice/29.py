60a086ac54c5ca27abccc3ed3828329047669974
Sunghan Shin <crossrunway01@gmail.com>
2024-03-20 02:05:49 +0900

    240319

---
+++ python/practice/29.py
@@ -0,0 +1,23 @@
+def solution(enroll, referral, seller, amount):
+    answer = []
+    parent = {}
+    total = {}
+    
+    for i in range(len(enroll)):
+        parent[enroll[i]] = referral[i]
+
+    for i in range(len(enroll)):
+        total[enroll[i]] = 0
+
+    for i in range(len(seller)):
+        money = amount[i] * 100
+        cur_name = seller[i]
+        while money > 0 and cur_name != "-":
+            total[cur_name] += money - money // 10
+            cur_name = parent[cur_name]
+            money //= 10
+
+    for name in total:
+        answer.append(total[name])
+
+    return answer
