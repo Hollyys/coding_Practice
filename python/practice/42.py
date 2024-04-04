eb4ecd70969050e5067c30a2f7f6ec663a89fd3d
Hollyys <crossrunway01@gmail.com>
2024-04-04 21:14:58 +0900

    240404: 분석 내용 추가

--- python/practice/42.py
+++ python/practice/42.py
@@ -14,6 +14,7 @@ def solution(maps):
     while q:
         pop = q.popleft()
         cn, cm, dist = pop[0], pop[1], pop[2]
+        # visited[cn][cm] = True
         if cn == n-1 and cm == m-1:
             return dist
         
@@ -24,3 +25,9 @@ def solution(maps):
                 q.append([wn, wm, dist+1])
         
     return -1
+
+# pop 하는 모든 좌표에 대해 방문 기록을 남기는 것 보다
+# append 하는 좌표에 대해서만 기록을 남기는 것이 낫다.
+# 왜냐하면 append 좌표는 특정 조건에 대해 한번 걸러진
+# 원소들이기 때문에 경로의 경우의 수가 매우 많아질 경우,
+# 연산량이 기하급수적으로 늘어나게 된다.
